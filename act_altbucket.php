27ac29912eb8b9a46a1c4e27453b565b7891bce4
Gwen <g@10degres.net>
2017-04-13 11:35:14 +0300

    altbucket

--- act_altbucket.php
+++ act_altbucket.php
@@ -47,6 +47,10 @@ if( $_SERVER['argc'] != 3 ) {
 	}
 
 	$db = $config->db = mysqli_connect( $config->parameters['database_host'], $config->parameters['database_user'], $config->parameters['database_password'], $config->parameters['database_name'] );
+	
+	define( 'SEPARATOR_KEYWORD', '__SEP__' );
+	$t_todo = [];
+	$t_separator = [ '.', '-' ];
 } //
 
 
@@ -64,7 +68,7 @@ if( $_SERVER['argc'] != 3 ) {
 } //
 
 
-{ // get known subdomains
+{ // get known buckets
 	$q = "SELECT * FROM arus_bucket AS b WHERE b.project_id='".$project_id."'";
 	$r = $db->query( $q );
 	if( !$r ) {
@@ -79,20 +83,61 @@ if( $_SERVER['argc'] != 3 ) {
 	//var_dump($final_file);
 	
 	while( ($bucket=$r->fetch_object()) ) {
-		file_put_contents( $bucket_file, str_replace('-','.',$bucket->name)."\n", FILE_APPEND );
+		$t_todo[] = preg_replace( '#[^0-9a-zA-Z]#i', SEPARATOR_KEYWORD, $bucket->name );
+		
 	}
 } //
 
+/*
+{ // get known subdomains, finally we don't want to apply variation (altdns) to subdomains
+	$q = "SELECT * FROM arus_host AS h WHERE h.project_id='".$project_id."'";
+	$r = $db->query( $q );
+	if( !$r ) {
+		usage( 'query error (get hosts)' );
+	}
+	
+	while( ($host=$r->fetch_object()) ) {
+		$t_todo[] = preg_replace( '#[^0-9a-zA-Z]#i', SEPARATOR_KEYWORD, $host->name );
+		//file_put_contents( $bucket_file, str_replace('-','.',$bucket->name)."\n", FILE_APPEND );
+	}
+} //
+*/
 
 { // alter bucket and create variations
+	$t_all = [];
+	foreach( $t_todo as $w ) {
+		foreach( $t_separator as $s ) {
+			$t_all[] = str_replace( SEPARATOR_KEYWORD, $s, $w );
+		}
+	}
+	file_put_contents( $bucket_file, implode("\n",$t_all), FILE_APPEND );
+
 	$cmd = '/opt/bin/altdns -i '.$bucket_file.' -o '.$combin_file.' -w '.$wordlist;
 	//echo $cmd."\n";
 	exec( $cmd );
-	
+
 	$t_combin = file( $combin_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES );
 	array_walk( $t_combin, function(&$v,$k){ $v=str_replace('.','-',$v);$v=trim($v,'-');} );
+} //
+
+
+{ // get known subdomains, and create some small variations
+	$t_todo = [];
+	$q = "SELECT * FROM arus_host AS h WHERE h.project_id='".$project_id."'";
+	$r = $db->query( $q );
+	if( !$r ) {
+		usage( 'query error (get hosts)' );
+	}
+	
+	while( ($host=$r->fetch_object()) ) {
+		$h = preg_replace( '#[^0-9a-zA-Z]#i', SEPARATOR_KEYWORD, $host->name );
+		foreach( $t_separator as $s ) {
+			$t_combin[] = str_replace( SEPARATOR_KEYWORD, $s, $h );
+		}
+	}
+	
 	$t_final = array_unique( $t_combin );
-	file_put_contents( $final_file, implode("\n",$t_final) );
+	file_put_contents( $final_file, implode("\n",$t_combin), FILE_APPEND );
 } //
 
 
